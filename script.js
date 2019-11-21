88ed3cec7de7c8718fad48f532ab82ea5dc1109d
Garza0 <konovalovandrey7@gmail.com>
2019-11-21 21:01:31 +0200

    chapter 3 added

--- script.js
+++ script.js
@@ -30,17 +30,57 @@
 
 //==Task 3
 
-let chessBoard = ''
-let size = 8
-for (let i = 1; i <= size; i++) {
+// let chessBoard = ''
+// let size = 8
+// for (let i = 1; i <= size; i++) {
 
-  for (let d = 1; d <= size; d++) {
-    if ((d + i) % 2 == 0) {
-      chessBoard += ' '
-    } else {
-      chessBoard += '#'
-    }
+//   for (let d = 1; d <= size; d++) {
+//     if ((d + i) % 2 == 0) {
+//       chessBoard += ' '
+//     } else {
+//       chessBoard += '#'
+//     }
+//   }
+//   chessBoard += '\n'
+// }
+// console.log(chessBoard)
+
+
+
+// =======CHAPTER 3=========
+//==Task 1 
+
+// function min(a, b) {
+//   if (a < b) return a
+//   else return b
+// }
+
+// console.log(min(0, 10));
+// console.log(min(0, -10));
+
+//==Task 2
+
+// function isEven(a) {
+//   if (a == 0) return true
+//   else if (a == 1) return false
+//   else if (a < 0) return isEven(-a)
+//   else return isEven(a - 2)
+// }
+
+// console.log(isEven(50))
+// console.log(isEven(75))
+// console.log(isEven(0))
+
+
+//==Task 3
+function countChar(str, char) {
+  let counter = 0
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) counter++
   }
-  chessBoard += '\n'
+  return counter
 }
-console.log(chessBoard)
+
+
+console.log(countChar("BBC", 'B'))
+console.log(countChar("kakkerlak", "k"))
